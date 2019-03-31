946d7e0c9c170635b8d69692eb05dc1d7ee6977e
hotsixredbull <akkalee1207@gmail.com>
2019-04-01 01:12:31 +0900

    addOffer added.

--- Server/config/config.js
+++ Server/config/config.js
@@ -10,8 +10,10 @@ module.exports = {
         {file:'./user_schema', collection:'User', schemaName:'UserSchema', modelName:'UserModel'},
 		{file:'./post_schema', collection:'Post', schemaName:'PostSchema', modelName:'PostModel'},
 		{file:'./order_schema', collection:'Order', schemaName:'OrderSchema', modelName:'OrderModel'},
+		{file:'./offer_schema', collection:'Offer', schemaName:'OfferSchema', modelName:'OfferModel'},
 		{file:'./country_schema', collection:'Country', schemaName:'CountrySchema', modelName:'CountryModel', defaultFile:'country.txt'},
 		{file:'./currency_schema', collection:'Currency', schemaName:'CurrencySchema', modelName:'CurrencyModel', defaultFile:'currency.txt'},
+		
 	],
 	route_info: [
         {file:'./post', path:'/process/addpost', method:'addpost', type:'post'},
@@ -30,7 +32,8 @@ module.exports = {
 		{file:'./browse', path:'/browse', method:'listBrowse', type:'get'},
 		{file:'./trades', path:'/trades', method:'addTrades', type:'post'},
 		{file:'./trades', path:'/trades', method:'listTrades', type:'get'},
-		{file:'./offers', path:'/offers', method:'addOffers', type:'post'},
+		{file:'./offers', path:'/addOffers', method:'addOffers', type:'post'},
+		{file:'./offers', path:'/add_offers', method:'addOffersPage', type:'get'},
 		{file:'./offers', path:'/offers', method:'listOffers', type:'get'},
 	],
 	facebook: {		// passport facebook
